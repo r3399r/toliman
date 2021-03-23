59df84be23a519a104b01c3d2e5909e71639a80a
yue.chang <r3399r@gmail.com>
2021-03-23 17:00:11 +0800

    add react-mathjax sample

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,22 @@
+import MathJax from 'react-mathjax';
+
 const Home = () => {
+  const tex =
+    '\\begin{array} {|r|r|}\\hline 0_0 & 0_1 & 0_2 \\\\ \\hline 1_0 & 1_1 & 1_2 \\\\ \\hline  \\end{array}';
+
   return (
     <div>
       <div>gigggi</div>
       <button className="uk-button">123</button>
+      <div>
+        <MathJax.Provider>
+          <div>
+            已知某方程式This is an inline math formula: <MathJax.Node inline={true} formula={tex} />
+            . And a block one:
+            <MathJax.Node formula={tex} />
+          </div>
+        </MathJax.Provider>
+      </div>
     </div>
   );
 };
