28dabdc7f9f40bd993b45ff28a0bc929ba566e92
yue.chang <r3399r@gmail.com>
2021-03-24 11:19:48 +0800

    add answer block

--- src/component/Textarea2MathJax.tsx
+++ src/component/Textarea2MathJax.tsx
@@ -2,16 +2,22 @@ import MathJax from 'react-mathjax';
 
 type Props = {
   text: string;
+  allowBlock?: boolean;
 };
 
-const Textarea2MathJax = ({ text }: Props) => {
+const Textarea2MathJax = ({ text, allowBlock = true }: Props) => {
   return (
     <div>
       <MathJax.Provider>
         {text.split('\n').map((line: string, index: number) => {
+          const splited: string[] = line.split('$$');
+
+          if (allowBlock === true && splited.length === 3 && splited[0] === '' && splited[2] === '')
+            return <MathJax.Node formula={splited[1]} key={index} />;
+
           return (
             <div key={index}>
-              {line.split('$$').map((val: string, cnt: number) => {
+              {splited.map((val: string, cnt: number) => {
                 if (cnt % 2 === 0) return val;
 
                 return <MathJax.Node inline={true} formula={val} key={cnt} />;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Textarea2MathJax from 'src/component/Textarea2MathJax';
 
 const Home = () => {
-  const [value, setValue] = useState<string>();
   const [countdown, setCountdown] = useState<NodeJS.Timeout>();
+  const [id, setId] = useState<string>();
 
-  const handleEvent = (ev: { target: HTMLTextAreaElement }) => {
+  const [question, setQuestion] = useState<string>();
+  const [isQueAreaDisable, setIsQueAreaDisable] = useState<boolean>(false);
+
+  const [answer, setAnswer] = useState<string>();
+  const [isAnsAreaDisable, setIsAnsAreaDisable] = useState<boolean>(false);
+
+  useEffect(() => {
+    setId((Date.now() * 1000 + Math.floor(Math.random() * 1000)).toString(16));
+  }, []);
+
+  const handleQuestionEvent = (ev: { target: HTMLTextAreaElement }) => {
+    setIsAnsAreaDisable(true);
     if (countdown) clearTimeout(countdown);
-    const newCountdown = setTimeout(() => setValue(ev.target.value), 600);
+    const newCountdown = setTimeout(() => {
+      setQuestion(ev.target.value);
+      setIsAnsAreaDisable(false);
+    }, 600);
     setCountdown(newCountdown);
   };
 
+  const handleAnswerEvent = (ev: { target: HTMLTextAreaElement }) => {
+    setIsQueAreaDisable(true);
+    if (countdown) clearTimeout(countdown);
+    const newCountdown = setTimeout(() => {
+      setAnswer(ev.target.value);
+      setIsQueAreaDisable(false);
+    }, 600);
+    setCountdown(newCountdown);
+  };
+
+  const result: string = JSON.stringify({ id, question, answer });
+
   return (
     <div>
-      <textarea className="uk-textarea" rows={4} placeholder="Textarea" onChange={handleEvent} />
-      {value && <Textarea2MathJax text={value} />}
+      <div>ID: {id}</div>
+      <div>題目</div>
+      <textarea
+        className="uk-textarea"
+        rows={4}
+        placeholder="Textarea"
+        onChange={handleQuestionEvent}
+        disabled={isQueAreaDisable}
+      />
+      <div>答案</div>
+      <textarea
+        className="uk-textarea"
+        rows={2}
+        placeholder="Textarea"
+        onChange={handleAnswerEvent}
+        disabled={isAnsAreaDisable}
+      />
+      {question && <Textarea2MathJax text={question} />}
+      {answer && <Textarea2MathJax text={answer} allowBlock={false} />}
+      <div>{result}</div>
+      <CopyToClipboard text={result}>
+        <button>Copy to clipboard with button</button>
+      </CopyToClipboard>
     </div>
   );
 };
