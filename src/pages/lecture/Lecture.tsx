025b558730431f13ee8a4c12fd7c275df8bb5694
yue.chang <r3399r@gmail.com>
2021-05-03 16:25:57 +0800

    revise design

--- src/pages/lecture/Lecture.tsx
+++ src/pages/lecture/Lecture.tsx
@@ -2,21 +2,21 @@ import Textarea2MathJax from 'src/component/Textarea2MathJax';
 import Concept from './component/Concept';
 import Example from './component/Example';
 import Exercise from './component/Exercise';
-import Note from './component/Note';
 import style from './Lecture.module.scss';
 
 const Lecture = () => {
   return (
     <div className={style.content}>
       <div className={style.head}>數與式</div>
-      <Concept>
+      <Concept title="乘法公式">
         <div className={style.conceptHead}>乘法公式</div>
-        <Note>s</Note>
-        <Textarea2MathJax
-          text={
-            'ps.推導$$(a+b)^2$$、$$(a-b)^2$$、$$(a+b)(a-b)$$、$$(a+b)^3$$、$$(a-b)^3$$、$$a^3+b^3$$、$$a^3-b^3$$'
-          }
-        />
+        <div className={style.note}>
+          <Textarea2MathJax
+            text={
+              'ps.推導$$(a+b)^2$$、$$(a-b)^2$$、$$(a+b)(a-b)$$、$$(a+b)^3$$、$$(a-b)^3$$、$$a^3+b^3$$、$$a^3-b^3$$'
+            }
+          />
+        </div>
       </Concept>
       <Exercise ids={['178f77a66b0', '178f77a9938', '178f77a66b0']} />
       <Example num={1} ids={['178f77a66b0', '178f77a9938']} />
--- src/pages/lecture/component/Concept.tsx
+++ src/pages/lecture/component/Concept.tsx
@@ -2,13 +2,14 @@ import { ReactNode } from 'react';
 import style from './Concept.module.scss';
 
 type Props = {
+  title: string;
   children: ReactNode;
 };
 
-const Concept = ({ children }: Props) => {
+const Concept = ({ title, children }: Props) => {
   return (
     <div className={style.content}>
-      <div className={style.title}>觀念</div>
+      <div className={style.title}>主題-{title}</div>
       <div className={style.card}>{children}</div>
     </div>
   );
