533017f9bf22a318bc6cef4b578da85e51d58352
yue.chang <r3399r@gmail.com>
2021-05-03 15:49:48 +0800

    remove Child

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,7 +4,6 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 const route = {
   Home: loadable(() => import('./pages/home/Home')),
   Edit: loadable(() => import('./pages/edit/Edit')),
-  Child: loadable(() => import('./pages/child/Child')),
   Hw: loadable(() => import('./pages/hw/Hw')),
   Lecture: loadable(() => import('./pages/lecture/Lecture')),
   Bank: loadable(() => import('./pages/bank/Bank')),
@@ -13,11 +12,10 @@ const route = {
 const AppRoutes = () => {
   return (
     <Switch>
-      <Route exact={true} path="/toliman/child" component={route.Child} />
-      <Route exact={true} path="/toliman/hw" component={route.Hw} />
-      <Route exact={true} path="/toliman/lecture" component={route.Lecture} />
-      <Route exact={true} path="/toliman/bank" component={route.Bank} />
       <Route exact={true} path="/toliman/edit" component={route.Edit} />
+      <Route exact={true} path="/toliman/bank" component={route.Bank} />
+      <Route exact={true} path="/toliman/lecture" component={route.Lecture} />
+      <Route exact={true} path="/toliman/hw" component={route.Hw} />
       <Route exact={true} path="/toliman" component={route.Home} />
       <Redirect to="/toliman" />
     </Switch>
