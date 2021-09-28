8f120578412686b976a294e1cf0b7363ac1d278b
yue.chang <r3399r@gmail.com>
2021-09-28 11:54:47 +0800

    improve ux for editor

--- src/pages/edit/Edit.tsx
+++ src/pages/edit/Edit.tsx
@@ -1,16 +1,14 @@
-import { Button, Checkbox, Input, Radio, RadioChangeEvent, Upload } from 'antd';
+import { Button, Checkbox, Input, message, Radio, RadioChangeEvent } from 'antd';
 import { useEffect, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Textarea2MathJax from 'src/component/Textarea2MathJax';
 import { chapterList, Question } from 'src/model/bank';
-import { getQuestion } from 'src/services/questionService';
 import style from './Edit.module.scss';
 
 const Home = () => {
   const [countdown, setCountdown] = useState<NodeJS.Timeout>();
   const [isRendering, setIsRendering] = useState<boolean>(false);
 
-  const [id, setId] = useState<string>('');
   const [randomId, setRandomId] = useState<string>('');
 
   const [chapter, setChapter] = useState<string>('');
@@ -19,7 +17,6 @@ const Home = () => {
   const [answer, setAnswer] = useState<string>('');
 
   const [hasImage, setHasImage] = useState<boolean>(false);
-  const [image, setImage] = useState<string>('');
 
   useEffect(() => {
     setRandomId(Date.now().toString(16));
@@ -35,18 +32,23 @@ const Home = () => {
   }, [question, answer]);
 
   const handleEdit = (ev: { target: HTMLInputElement }) => {
-    const inputId: string = ev.target.value;
-    const res: Question | undefined = getQuestion(inputId);
+    try {
+      const inputJSON: string = ev.target.value;
+      const res: Question = JSON.parse(inputJSON);
 
-    if (inputId === '') setId('');
-    else if (res === undefined) alert(`id: ${inputId} 不存在`);
-    else {
-      setId(inputId);
+      if (
+        res.question === undefined ||
+        res.question === undefined ||
+        res.answer === undefined ||
+        res.hasImage === undefined
+      )
+        message.error('格式不符');
       setChapter(res.chapter);
       setQuestion(res.question);
       setAnswer(res.answer);
       setHasImage(res.hasImage);
-      if (res.hasImage) setImage(`toliman/images/${inputId}.PNG`);
+    } catch (e) {
+      message.error('格式不符');
     }
   };
 
@@ -63,24 +65,21 @@ const Home = () => {
   };
 
   const onCheckboxClick = () => {
-    if (!hasImage === false) setImage('');
     setHasImage(!hasImage);
   };
 
-  const displayImage = (file: File) => {
-    setImage(URL.createObjectURL(file));
-
-    return false;
+  const onCopy = () => {
+    message.success('已複製');
   };
 
-  const result: Question = { id: id === '' ? randomId : id, chapter, question, answer, hasImage };
+  const result: Question = { id: randomId, chapter, question, answer, hasImage };
 
   return (
     <div className={style.content}>
       <div>修改</div>
       <Input placeholder="貼上JSON" onChange={handleEdit} />
       <div>章節</div>
-      <Radio.Group options={chapterList} onChange={handleCheckbox} />
+      <Radio.Group options={chapterList} onChange={handleCheckbox} value={chapter} />
       <div>題目</div>
       <Input.TextArea
         rows={4}
@@ -91,19 +90,13 @@ const Home = () => {
       <div>答案</div>
       <Input.TextArea rows={2} placeholder="輸入答案" onChange={handleAnswerEvent} value={answer} />
       <div>
-        <Checkbox id="hasImage" onClick={onCheckboxClick}>
+        <Checkbox id="hasImage" checked={hasImage} onClick={onCheckboxClick}>
           是否有圖
         </Checkbox>
       </div>
-      {hasImage && (
-        <Upload beforeUpload={displayImage} maxCount={1}>
-          <Button>選擇檔案</Button>
-        </Upload>
-      )}
       <hr />
       <div className={style.preview}>
         {isRendering === true && <div>產生中...</div>}
-        {image !== '' && <img className={style.image} src={image} alt="" role="presentation" />}
         {isRendering === false && question && <Textarea2MathJax text={question} />}
         {isRendering === false && answer && (
           <Textarea2MathJax className={style.ans} text={'Ans: ' + answer} allowBlock={false} />
@@ -112,7 +105,7 @@ const Home = () => {
       <hr />
       <div>{JSON.stringify(result)}</div>
       <div>
-        <CopyToClipboard text={JSON.stringify(result)}>
+        <CopyToClipboard text={JSON.stringify(result)} onCopy={onCopy}>
           <Button>複製文字</Button>
         </CopyToClipboard>
       </div>
