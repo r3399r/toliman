241cb0a45ebf0e0c522b219f12955a06530c01f0
yue.chang <r3399r@gmail.com>
2021-11-05 19:22:24 +0800

    lecture version of review B4

--- src/pages/lecture/review/B4C11SpaceConcept.tsx
+++ src/pages/lecture/review/B4C11SpaceConcept.tsx
@@ -3,96 +3,167 @@ import style from 'src/pages/lecture/Common.module.scss';
 import Concept from 'src/pages/lecture/component/Concept';
 import Example from 'src/pages/lecture/component/Example';
 import Exercise from 'src/pages/lecture/component/Exercise';
+import Page from 'src/pages/lecture/component/Page';
 
 const B4C11SpaceConcept = () => {
   return (
     <div>
-      <div className={style.head}>Ch11 空間概念</div>
-      <Concept num={1} title="空間中直線與平面之間的關係">
-        <Textarea2MathJax
-          text={
-            '空間中的直線與平面均為無限延伸，沒有邊界，相異兩點可決定一條直線、不共線三點可決定一個平面，它們的關係如下：'
-          }
-        />
-        <Textarea2MathJax text={'(1) 平面與平面：兩平面可「平行」、「重合」、「交於一線」'} />
-        <Textarea2MathJax
-          text={'(2) 直線與平面：一條直線與平面可「平行」、「交於一點」、「線在平面上」'}
-        />
-        <Textarea2MathJax
-          text={'(3) 直線與直線：兩條直線可「平行」、「交於一點」、「重合」、「歪斜」'}
-        />
-      </Concept>
-      <Exercise ids={['17cdff745aa', '17cdff84f8b']} />
-      <Concept num={2} title="二面角">
-        <Textarea2MathJax
-          text={
-            '將平面沿著面上的直線來彎折，所成圖形為二面角。射線 $\\overrightarrow{AB}$ 與射線 $\\overrightarrow{AC}$ 均與折邊垂直，則 $\\angle BAC=\\theta$ 為二面角，範圍為 $0\\degree\\le\\theta\\le180\\degree$。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17cdff9458e', '17cdffa2246']} />
-      <Concept num={3} title="三垂線定理">
-        <Textarea2MathJax
-          text={
-            '如圖，直線 $\\overleftrightarrow{AB}$ 垂直平面 $E$ 於 $B$ 點，直線 $L$ 在平面 $E$ 上，若 $C\\in L$ 且 $\\overline{BC}\\perp L$，則 $\\overline{AC}$ 與 $L$ 也互相垂直。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17cdffd7863', '17cdffec15a']} />
-      <Concept num={4} title="空間中使用畢氏定理">
-        <Textarea2MathJax
-          text={
-            '我們常在立體圖形的剖面圖中察覺到直角三角形，我們常用畢氏定理進行基本的長度計算。如長方體的三邊長為 $a$、$b$、$c$，則內部對角線長度為'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce002d564', '17ce003c076']} />
-      <Concept num={5} title="錐體體積">
-        <Textarea2MathJax
-          text={
-            '柱體上下兩面平行且全等，將其中一面縮成一點即為錐體，體積均為柱體的三分之一，即「底面積$\\times$高$\\times\\frac13$」。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce0001ef1', '17ce0013699']} />
-      <Concept num={6} title="空間坐標系">
-        <Textarea2MathJax
-          text={
-            '空間中每一點用 $(x,y,z)$ 表示，期距離公式、中點坐標、分點坐標均與平面坐標系雷同。設 $A(x_1,y_1,z_1)$，$B(x_2,y_2,z_2)$，則 $\\overline{AB}=\\sqrt{(x_1-x_2)^2+(y_1-y_2)^2+(x_3-y_3)^2}$'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce0065827', '17ce007f2d9']} />
-      <Concept num={7} title="球面與地球的經緯線(數B)">
-        <Textarea2MathJax
-          text={'(1) 球面：空間中到球心的距離為 $r$ 的所有點形成的圖形為球面，稱 $r$ 為半徑。'}
-        />
-        <Textarea2MathJax
-          text={
-            '(2) 地球的經線與緯線：地球可視為球體，半徑約 $6400$ 公里，經線為通過南北極的大圓，分為東經與西經各 $180\\degree$；緯線為與赤道面平行的小圓，分為南緯與北緯各 $90\\degree$。若地球半徑為 $R$，則緯度 $\\theta$ 的緯線是半徑為 $R\\cos\\theta$ 的小圓。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce008fbb5', '17ce00a154d']} />
-      <Concept num={8} title="圓錐曲線(數B)">
-        <Textarea2MathJax
-          text={
-            '空間中有一直圓錐面，探討與平面香蕉所截出的圖形，總共有七種，可分為三大類：斜率小於圓錐面、斜率等於圓錐面、斜率大於圓錐面：'
-          }
-        />
-        <Textarea2MathJax
-          text={
-            '(1) 斜率小於圓錐面：橢圓、圓、一點\n(2) 斜率等於圓錐面：拋物線、一直線\n(3) 斜率大於圓錐面：雙曲線、兩相交直線'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce00c5e42']} />
-      <Example num={1} ids={['17cebe71628', '17cebe8f1c9']} />
-      <Example num={2} ids={['17cebe91c86', '17cebe9df49']} />
-      <Example num={3} ids={['17cebeaa5b5', '17cebeb1bce']} />
-      <Example num={4} ids={['17cebec7ace', '17cebed7be4']} />
-      <Example num={5} ids={['17cebf09d4e', '17cea2e6483']} />
-      <Example num={6} ids={['17cec34ee97', '17cec384cda']} note=" (數B)" />
+      <Page pageNum={1}>
+        <div className={style.head}>Ch11 空間概念</div>
+        <Concept num={1} title="空間中直線與平面之間的關係" height="6.6cm">
+          <Textarea2MathJax
+            text={
+              '空間中的直線與平面均為無限延伸，沒有邊界，相異兩點可決定一條直線、不共線三點可決定一個平面，它們的關係如下：'
+            }
+          />
+          <Textarea2MathJax text={'(1) 平面與平面：兩平面可「平行」、「重合」、「交於一線」'} />
+          <Textarea2MathJax
+            text={'(2) 直線與平面：一條直線與平面可「平行」、「交於一點」、「線在平面上」'}
+          />
+          <Textarea2MathJax
+            text={'(3) 直線與直線：兩條直線可「平行」、「交於一點」、「重合」、「歪斜」'}
+          />
+        </Concept>
+        <Exercise ids={['17cdff745aa', '17cdff84f8b']} height="8.5cm" />
+        <Concept num={2} title="二面角">
+          <img
+            style={{ float: 'right', width: '1.9cm' }}
+            src="/toliman/images/二面角.PNG"
+            alt=""
+            role="presentation"
+          />
+          <Textarea2MathJax
+            text={
+              '將平面沿著面上的直線來彎折，所成圖形為二面角。射線 $\\overrightarrow{AB}$ 與射線 $\\overrightarrow{AC}$ 均與折邊垂直，則 $\\angle BAC=\\theta$ 為二面角，範圍為 $0\\degree\\le\\theta\\le180\\degree$。'
+            }
+          />
+        </Concept>
+      </Page>
+      <Page pageNum={2}>
+        <Exercise ids={['17cdff9458e', '17cdffa2246']} height="9cm" />
+        <Concept num={3} title="三垂線定理">
+          <img
+            style={{ float: 'right', width: '5cm' }}
+            src="/toliman/images/三垂線定理.PNG"
+            alt=""
+            role="presentation"
+          />
+          <Textarea2MathJax
+            text={
+              '如圖，直線 $\\overleftrightarrow{AB}$ 垂直平面 $E$ 於 $B$ 點，直線 $L$ 在平面 $E$ 上，若 $C\\in L$ 且 $\\overline{BC}\\perp L$，則 $\\overline{AC}$ 與 $L$ 也互相垂直。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17cdffd7863', '17cdffec15a']} height="9cm" />
+      </Page>
+      <Page pageNum={3}>
+        <Concept num={4} title="空間中使用畢氏定理" height="3.4cm">
+          <Textarea2MathJax
+            text={
+              '我們常在立體圖形的剖面圖中察覺到直角三角形，我們常用畢氏定理進行基本的長度計算。如長方體的三邊長為 $a$、$b$、$c$，則內部對角線長度為'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce002d564', '17ce003c076']} height="7.5cm" />
+        <Concept num={5} title="錐體體積">
+          <Textarea2MathJax
+            text={
+              '柱體上下兩面平行且全等，將其中一面縮成一點即為錐體，體積均為柱體的三分之一，即「底面積$\\times$高$\\times\\frac13$」。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce0001ef1', '17ce0013699']} />
+      </Page>
+      <Page pageNum={4}>
+        <Concept num={6} title="空間坐標系">
+          <Textarea2MathJax
+            text={
+              '空間中每一點用 $(x,y,z)$ 表示，期距離公式、中點坐標、分點坐標均與平面坐標系雷同。設 $A(x_1,y_1,z_1)$，$B(x_2,y_2,z_2)$，則 $\\overline{AB}=\\sqrt{(x_1-x_2)^2+(y_1-y_2)^2+(x_3-y_3)^2}$'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce0065827', '17ce007f2d9']} height="7cm" />
+        <Concept num={7} title="球面與地球的經緯線(數B)">
+          <Textarea2MathJax
+            text={'(1) 球面：空間中到球心的距離為 $r$ 的所有點形成的圖形為球面，稱 $r$ 為半徑。'}
+          />
+          <Textarea2MathJax
+            text={
+              '(2) 地球的經線與緯線：地球可視為球體，半徑約 $6400$ 公里，經線為通過南北極的大圓，分為東經與西經各 $180\\degree$；緯線為與赤道面平行的小圓，分為南緯與北緯各 $90\\degree$。若地球半徑為 $R$，則緯度 $\\theta$ 的緯線是半徑為 $R\\cos\\theta$ 的小圓。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce008fbb5', '17ce00a154d']} height="7cm" />
+      </Page>
+      <Page pageNum={5}>
+        <Concept num={8} title="圓錐曲線(數B)">
+          <Textarea2MathJax
+            text={'空間中有一直圓錐面，探討與平面香蕉所截出的圖形，總共有七種，可分為三類：'}
+          />
+          <Textarea2MathJax
+            text={
+              '(1) 斜率小於圓錐面：橢圓、圓、一點\n(2) 斜率等於圓錐面：拋物線、一直線\n(3) 斜率大於圓錐面：雙曲線、兩相交直線'
+            }
+          />
+          <div style={{ display: 'flex' }}>
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-橢圓.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-圓.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-點.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-拋物線.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-一直線.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-雙曲線.PNG"
+              alt=""
+              role="presentation"
+            />
+            <img
+              style={{ width: '14.2%' }}
+              src="/toliman/images/二次曲線-兩直線.PNG"
+              alt=""
+              role="presentation"
+            />
+          </div>
+        </Concept>
+        <Exercise ids={['17ce00c5e42']} />
+      </Page>
+      <Page pageNum={6}>
+        <Example num={1} ids={['17cebe71628', '17cebe8f1c9']} />
+        <Example num={2} ids={['17cebe91c86', '17cebe9df49']} />
+      </Page>
+      <Page pageNum={7}>
+        <Example num={3} ids={['17cebeaa5b5', '17cebeb1bce']} />
+        <Example num={4} ids={['17cebec7ace', '17cebed7be4']} />
+      </Page>
+      <Page pageNum={8}>
+        <Example num={5} ids={['17cebf09d4e', '17cea2e6483']} />
+        <Example num={6} ids={['17cec34ee97', '17cec384cda']} note=" (數B)" />
+      </Page>
     </div>
   );
 };
--- src/pages/lecture/review/B4C14Matrix.tsx
+++ src/pages/lecture/review/B4C14Matrix.tsx
@@ -3,110 +3,129 @@ import style from 'src/pages/lecture/Common.module.scss';
 import Concept from 'src/pages/lecture/component/Concept';
 import Example from 'src/pages/lecture/component/Example';
 import Exercise from 'src/pages/lecture/component/Exercise';
+import Page from 'src/pages/lecture/component/Page';
 
 const B4C14Matrix = () => {
   return (
     <div>
-      <div className={style.head}>Ch14 矩陣</div>
-      <Concept num={1} title="矩陣的元與階數">
-        <Textarea2MathJax
-          text={
-            '矩陣為中括號內 $m$ 列 $n$ 行的數字組，並稱 $m\\times n$ 為矩陣的「階數」。而 $n\\times n$ 階的矩陣會稱為「$n$ 階方陣」。矩陣中第 $i$ 列第 $j$ 行的數值稱為「第 $(i,j)$ 元」。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce407e041', '17ce408fc10']} />
-      <Concept num={2} title="利用列運算解聯立方程式">
-        <Textarea2MathJax
-          text={
-            '未知數的係數即常數所組成的矩陣稱為增廣矩陣，將增廣矩陣有系統地使用列運算可解出未知數，解會有以下三種情況：\n(1) 唯一解 $\\begin{bmatrix}1&0&0&a\\\\0&1&0&b\\\\0&0&1&c\\end{bmatrix}$ (2) 無解 $\\begin{bmatrix}0&0&0&k\\ne0\\end{bmatrix}$ (3) 無限多解 $\\begin{bmatrix}0&0&0&0\\end{bmatrix}$'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce40ae751', '17ce40c83ce']} />
-      <Concept num={3} title="矩陣的相等、加減與係數積">
-        <Textarea2MathJax
-          text={
-            '同階數的矩陣 $A$ 與 $B$，若各元對應相等則 $A=B$。同階數的矩陣才可以加減，矩陣 $A$ 的每一元乘上常數 $r$ 即為 $rA$。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce40d9768', '17ce40fb208']} />
-      <Concept num={4} title="矩陣的相乘">
-        <Textarea2MathJax
-          text={
-            '$n\\times r$ 階矩陣可乘上 $r\\times k$ 階矩陣，得到 $n\\times k$ 階矩陣。請由實際操作中學習如何運算。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce410b897', '17ce412dcb2']} />
-      <Concept num={5} title="二階反方陣">
-        <Textarea2MathJax
-          text={
-            '設方陣 $A=\\begin{bmatrix}p&q\\\\r&s\\end{bmatrix}$，規定 $\\det(A)=\\begin{vmatrix}p&q\\\\r&s\\end{vmatrix}$，若 $\\det(A)\\ne0$，則 $A$ 有乘法反方陣 $A^{-1}=\\frac1{\\det(A)}\\begin{bmatrix}s&-q\\\\-r&p\\end{bmatrix}$，滿足 $AA^{-1}=A^{-1}A=\\begin{bmatrix}1&0\\\\0&1\\end{bmatrix}$，稱 $I_2=\\begin{bmatrix}1&0\\\\0&1\\end{bmatrix}$ 為單位方陣。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce4148e4f', '17ce4154725']} />
-      <Concept num={6} title="矩陣運算的性質">
-        <Textarea2MathJax
-          text={
-            '(1) 結合律：$(A+B)+C=A+(B+C)$、$(AB)C=A(BC)$\n(2) 分配律：$A(B+C)=AB+AC$、$(A+B)C=AC+BC$\n(3) 交換律：$A+B=B+A$、$AB\\ne BA$ (一般矩陣沒有乘法交換律)'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce4166477', '17ce4181410']} />
-      <Concept num={7} title="平面的線性轉換">
-        <Textarea2MathJax
-          text={
-            '平面的點 $(x,y)$ 寫成行矩陣，則二階方陣 $A$ 乘 $\\begin{bmatrix}x\\\\y\\end{bmatrix}$ 的結果代表點 $(x,y)$ 經方陣 $A$ 變換所得的「像」，稱為線性變換。平面上的直線經線性變換後，其圖形必為直線或一點，而封閉區域的圖形經 $A$ 變換後，新圖形的面積為原面積乘上 $|\\det(A)|$。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce41af316']} />
-      <Concept num={8} title="特殊的線性轉換 (數A)">
-        <Textarea2MathJax
-          text={
-            '(1) 旋轉方陣：$\\begin{bmatrix}\\cos\\theta&-\\sin\\theta\\\\\\sin\\theta&\\cos\\theta\\end{bmatrix}$ 可把點 $(x,y)$ 逆時針繞原點旋轉 $\\theta$。'
-          }
-        />
-        <Textarea2MathJax
-          text={
-            '(2) 鏡射方陣：$\\begin{bmatrix}\\cos2\\theta&\\sin2\\theta\\\\\\sin2\\theta&-\\cos2\\theta\\end{bmatrix}$ 把點 $(x,y)$ 對直線 $y=\\tan\\theta\\cdot x$ 鏡射。'
-          }
-        />
-        <Textarea2MathJax
-          text={
-            '(3) 伸縮方陣：$\\begin{bmatrix}a&0\\\\0&b\\end{bmatrix}$ 把點 $(x,y)$ 以 $x$ 軸為準左右伸縮為 $a$ 倍，再以 $y$ 軸為準上下伸縮為 $b$ 倍。'
-          }
-        />
-        <Textarea2MathJax
-          text={
-            '(4) 推移方陣：$\\begin{bmatrix}1&k\\\\0&1\\end{bmatrix}$ 把點 $(x,y)$ 朝 $x$ 軸方向推移 $y$ 坐標的 $k$ 倍；而 $\\begin{bmatrix}1&0\\\\k&1\\end{bmatrix}$ 把點 $(x,y)$ 朝 $y$ 軸方向推移 $x$ 坐標的 $k$ 倍。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce41d7778', '17ce41ee1d6']} />
-      <Concept num={9} title="轉移矩陣 (數A)">
-        <Textarea2MathJax
-          text={
-            '(1) 行矩陣序列：若序列 $\\begin{bmatrix}a_1\\\\b_1\\end{bmatrix}$、$\\begin{bmatrix}a_2\\\\b_2\\end{bmatrix}$、$\\begin{bmatrix}a_3\\\\b_3\\end{bmatrix}$、... 的前後關係滿足 $\\begin{cases}a_{n+1}=pa_n+qb_n\\\\b_{n+1}=ra_n+sb_n\\end{cases}$，則可表為 $\\begin{bmatrix}a_{n+1}\\\\b_{n+1}\\end{bmatrix}=\\begin{bmatrix}p&q\\\\r&s\\end{bmatrix}=\\begin{bmatrix}a_n\\\\b_n\\end{bmatrix}$，相當於行矩陣的遞迴關係。'
-          }
-        />
-        <Textarea2MathJax
-          text={
-            '(2) 轉移矩陣：各元均非負，且任意行的元素和均為 $1$ 的矩陣，稱為轉移矩陣。若 $A$ 與 $B$ 為轉移矩陣，則 $AB$ 也是轉移矩陣。轉移矩陣可用於計算每個階段各種狀態的機率分布。'
-          }
-        />
-      </Concept>
-      <Exercise ids={['17ce42281ee', '17ce4249a61']} />
-      <Example num={1} ids={['17ce8eb133f', '17ce8edc471']} />
-      <Example num={2} ids={['17ce8ef7479', '17ce8f0c2a5']} />
-      <Example num={3} ids={['17ce8f3435f', '17ce8f4743b']} />
-      <Example num={4} ids={['17ce8f5a3d0', '17ce8f75d11']} />
-      <Example num={5} ids={['17ce8f8f1d1', '17ce8fa11a1']} />
-      <Example num={6} ids={['17ce8fc69b2', '17ce8fdee76']} />
-      <Example num={7} ids={['17ce905c7c2', '17ce9083c89']} />
+      <Page pageNum={26}>
+        <div className={style.head}>Ch14 矩陣</div>
+        <Concept num={1} title="矩陣的元與階數">
+          <Textarea2MathJax
+            text={
+              '矩陣為中括號內 $m$ 列 $n$ 行的數字組，並稱 $m\\times n$ 為矩陣的「階數」。而 $n\\times n$ 階的矩陣會稱為「$n$ 階方陣」。矩陣中第 $i$ 列第 $j$ 行的數值稱為「第 $(i,j)$ 元」。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce407e041', '17ce408fc10']} />
+        <Concept num={2} title="利用列運算解聯立方程式">
+          <Textarea2MathJax
+            text={
+              '未知數的係數即常數所組成的矩陣稱為增廣矩陣，將增廣矩陣有系統地使用列運算可解出未知數，解會有以下三種情況：\n(1) 唯一解 $\\begin{bmatrix}1&0&0&a\\\\0&1&0&b\\\\0&0&1&c\\end{bmatrix}$ (2) 無解 $\\begin{bmatrix}0&0&0&k\\ne0\\end{bmatrix}$ (3) 無限多解 $\\begin{bmatrix}0&0&0&0\\end{bmatrix}$'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce40ae751', '17ce40c83ce']} height="6.5cm" />
+      </Page>
+      <Page pageNum={27}>
+        <Concept num={3} title="矩陣的相等、加減與係數積">
+          <Textarea2MathJax
+            text={
+              '同階數的矩陣 $A$ 與 $B$，若各元對應相等則 $A=B$。同階數的矩陣才可以加減，矩陣 $A$ 的每一元乘上常數 $r$ 即為 $rA$。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce40d9768', '17ce40fb208']} height="7cm" />
+        <Concept num={4} title="矩陣的相乘">
+          <Textarea2MathJax
+            text={
+              '$n\\times r$ 階矩陣可乘上 $r\\times k$ 階矩陣，得到 $n\\times k$ 階矩陣。請由實際操作中學習如何運算。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce410b897', '17ce412dcb2']} height="9cm" />
+      </Page>
+      <Page pageNum={28}>
+        <Concept num={5} title="二階反方陣">
+          <Textarea2MathJax
+            text={
+              '設方陣 $A=\\begin{bmatrix}p&q\\\\r&s\\end{bmatrix}$，規定 $\\det(A)=\\begin{vmatrix}p&q\\\\r&s\\end{vmatrix}$，若 $\\det(A)\\ne0$，則 $A$ 有乘法反方陣 $A^{-1}=\\frac1{\\det(A)}\\begin{bmatrix}s&-q\\\\-r&p\\end{bmatrix}$，滿足 $AA^{-1}=A^{-1}A=I_2=\\begin{bmatrix}1&0\\\\0&1\\end{bmatrix}$，稱 $I_2$ 為單位方陣。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce4148e4f', '17ce4154725']} height="6.6cm" />
+        <Concept num={6} title="矩陣運算的性質">
+          <Textarea2MathJax
+            text={
+              '(1) 結合律：$(A+B)+C=A+(B+C)$、$(AB)C=A(BC)$\n(2) 分配律：$A(B+C)=AB+AC$、$(A+B)C=AC+BC$\n(3) 交換律：$A+B=B+A$、$AB\\ne BA$ (一般矩陣沒有乘法交換律)'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce4166477', '17ce4181410']} height="7cm" />
+      </Page>
+      <Page pageNum={29}>
+        <Concept num={7} title="平面的線性轉換">
+          <Textarea2MathJax
+            text={
+              '平面的點 $(x,y)$ 寫成行矩陣，則二階方陣 $A$ 乘 $\\begin{bmatrix}x\\\\y\\end{bmatrix}$ 的結果代表點 $(x,y)$ 經方陣 $A$ 變換所得的「像」，稱為線性變換。平面上的直線經線性變換後，其圖形必為直線或一點，而封閉區域的圖形經 $A$ 變換後，新圖形的面積為原面積乘上 $|\\det(A)|$。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce41af316']} height="8cm" />
+        <Concept num={8} title="特殊的線性轉換 (數A)">
+          <Textarea2MathJax
+            text={
+              '(1) 旋轉方陣：$\\begin{bmatrix}\\cos\\theta&-\\sin\\theta\\\\\\sin\\theta&\\cos\\theta\\end{bmatrix}$ 可把點 $(x,y)$ 逆時針繞原點旋轉 $\\theta$。'
+            }
+          />
+          <Textarea2MathJax
+            text={
+              '(2) 鏡射方陣：$\\begin{bmatrix}\\cos2\\theta&\\sin2\\theta\\\\\\sin2\\theta&-\\cos2\\theta\\end{bmatrix}$ 把點 $(x,y)$ 對直線 $y=\\tan\\theta\\cdot x$ 鏡射。'
+            }
+          />
+          <Textarea2MathJax
+            text={
+              '(3) 伸縮方陣：$\\begin{bmatrix}a&0\\\\0&b\\end{bmatrix}$ 把點 $(x,y)$ 以 $x$ 軸為準左右伸縮為 $a$ 倍，再以 $y$ 軸為準上下伸縮為 $b$ 倍。'
+            }
+          />
+          <Textarea2MathJax
+            text={
+              '(4) 推移方陣：$\\begin{bmatrix}1&k\\\\0&1\\end{bmatrix}$ 把點 $(x,y)$ 朝 $x$ 軸方向推移 $y$ 坐標的 $k$ 倍；而 $\\begin{bmatrix}1&0\\\\k&1\\end{bmatrix}$ 把點 $(x,y)$ 朝 $y$ 軸方向推移 $x$ 坐標的 $k$ 倍。'
+            }
+          />
+        </Concept>
+      </Page>
+      <Page pageNum={30}>
+        <Exercise ids={['17ce41d7778', '17ce41ee1d6']} height="8.7cm" />
+        <Concept num={9} title="轉移矩陣 (數A)">
+          <Textarea2MathJax
+            text={
+              '(1) 行矩陣序列：若序列 $\\begin{bmatrix}a_1\\\\b_1\\end{bmatrix}$、$\\begin{bmatrix}a_2\\\\b_2\\end{bmatrix}$、$\\begin{bmatrix}a_3\\\\b_3\\end{bmatrix}$、... 的前後關係滿足 $\\begin{cases}a_{n+1}=pa_n+qb_n\\\\b_{n+1}=ra_n+sb_n\\end{cases}$，則可表為 $\\begin{bmatrix}a_{n+1}\\\\b_{n+1}\\end{bmatrix}=\\begin{bmatrix}p&q\\\\r&s\\end{bmatrix}=\\begin{bmatrix}a_n\\\\b_n\\end{bmatrix}$，相當於行矩陣的遞迴關係。'
+            }
+          />
+          <Textarea2MathJax
+            text={
+              '(2) 轉移矩陣：各元均非負，且任意行的元素和均為 $1$ 的矩陣，稱為轉移矩陣。若 $A$ 與 $B$ 為轉移矩陣，則 $AB$ 也是轉移矩陣。轉移矩陣可用於計算每個階段各種狀態的機率分布。'
+            }
+          />
+        </Concept>
+        <Exercise ids={['17ce42281ee', '17ce4249a61']} height="7.4cm" />
+      </Page>
+      <Page pageNum={31}>
+        <Example num={1} ids={['17ce8eb133f', '17ce8edc471']} height="23cm" />
+      </Page>
+      <Page pageNum={32}>
+        <Example num={2} ids={['17ce8ef7479', '17ce8f0c2a5']} />
+        <Example num={3} ids={['17ce8f3435f', '17ce8f4743b']} />
+      </Page>
+      <Page pageNum={33}>
+        <Example num={4} ids={['17ce8f5a3d0', '17ce8f75d11']} />
+        <Example num={5} ids={['17ce8f8f1d1', '17ce8fa11a1']} />
+      </Page>
+      <Page pageNum={34}>
+        <Example num={6} ids={['17ce8fc69b2', '17ce8fdee76']} />
+        <Example num={7} ids={['17ce905c7c2', '17ce9083c89']} note=" (數A)" />
+      </Page>
     </div>
   );
 };
