b11ffb7ecd62d16390737eac250b7156d831de9d
yue.chang <r3399r@gmail.com>
2021-11-09 10:31:35 +0800

    add github action process

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
       <div className={style.button}>
         <Button onClick={onClick('test')}>考卷</Button>
       </div>
-      <div>Version 3.4</div>
+      <div>Version 3.5</div>
     </div>
   );
 };
